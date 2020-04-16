280f858b1de6cf2709b2ca2ad08cc5b9986b473f
Pratyay <praty94@gmail.com>
2020-04-16 10:44:32 +0530

    Added Material UI package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Cards,Charts,CountryPicker,Wrapper} from './Components';
+import { Cards, Charts, CountryPicker, Wrapper } from './Components';
 import styles from './App.module.css';
-class App extends React.Component{
-render(){
-  return (
-    <Wrapper className={styles.container}>
-      <Cards></Cards>
-      <Charts></Charts>
-      <CountryPicker></CountryPicker>
-    </Wrapper>
-  );
-}
+import { fetchData } from './api';
+class App extends React.Component {
+  state={
+    summary:{}
+  }
+  render() {
+    return (
+      <Wrapper className={styles.container}>
+        <Cards></Cards>
+        <Charts></Charts>
+        <CountryPicker></CountryPicker>
+      </Wrapper>
+    );
+  }
+  async componentDidMount() {
+    var response = await fetchData();
+    this.setState({summary:response});
+    console.log(response);
+  }
 }
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try{
+        const response = await axios.get(url);
+        return response;
+    }catch{
+        console.error("[fetchData] Failed");
+    }    
+}
+
+export const fetchCountries = async () => {
+    try{
+        const response = await axios.get(url+"/countries");
+        return response;
+    }catch{
+        console.error("[fetchCountries] Failed");
+    }
+}
