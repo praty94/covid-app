788eff1f053bdc9dd2b4eed54ee1ccaae9caea75
Pratyay <praty94@gmail.com>
2020-04-21 17:28:15 +0530

    Updates

---
+++ precache-manifest.1fa4d0cdfd61270e79c9572c2b433abe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b56264f9d69494b9eeb811305d829e8",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "fedb6bfba1990b7c54bf",
+    "url": "/covid-app/static/css/main.72be434f.chunk.css"
+  },
+  {
+    "revision": "4e9c234fe23a2710ecf8",
+    "url": "/covid-app/static/js/2.f6e8df48.chunk.js"
+  },
+  {
+    "revision": "fa5b1577a417bfc81cd27bc5dbbf1b42",
+    "url": "/covid-app/static/js/2.f6e8df48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedb6bfba1990b7c54bf",
+    "url": "/covid-app/static/js/main.21b0c818.chunk.js"
+  },
+  {
+    "revision": "a5376666c9d162bac84e",
+    "url": "/covid-app/static/js/runtime-main.848bdf1b.js"
+  },
+  {
+    "revision": "df488cb3f2ef667782bdb103da3a563a",
+    "url": "/covid-app/static/media/confirmed.df488cb3.svg"
+  },
+  {
+    "revision": "4e113c7e2f71c27ff4f71c6d1444df6c",
+    "url": "/covid-app/static/media/deaths.4e113c7e.svg"
+  },
+  {
+    "revision": "011ed21cfaeacee3cb24765d222d17bd",
+    "url": "/covid-app/static/media/recovered.011ed21c.svg"
+  }
+]);
